99ac1fe2a59d11a45c08c7942d5fc92242f5de5d
tusharshawarma <91686218+tusharshawarma@users.noreply.github.com>
2024-07-17 21:14:27 +0000

    Datatype conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let newScore = Number(score);
+// console.log(typeof newScore);
+// console.log(newScore);
+
+//"33" => 33
+//"33abc" => NaN
+// undefined => NaN
+// null => 0
+
+let isLoggedIn = ""
+
+let newIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof newIsLoggedIn);
+console.log(newIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "something something" => true
+
+
